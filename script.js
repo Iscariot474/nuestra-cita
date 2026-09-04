658f6e157436400fc321201c322e56715de7a301
Judas <123628394+Iscariot474@users.noreply.github.com>
2026-09-04 15:38:51 +0100

    Add pagination functionality in script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+let currentPage = 1;
+
+function nextPage() {
+
+    document
+        .getElementById(`page${currentPage}`)
+        .classList.remove("active");
+
+    currentPage++;
+
+    document
+        .getElementById(`page${currentPage}`)
+        .classList.add("active");
+}
